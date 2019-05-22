8339f4ee53bc624e2bf11cf270954e3003910916
maginamu <magi.namu@gmail.com>
2019-05-23 00:54:33 +0900

    test

--- python_learning/exceltest.py
+++ python_learning/exceltest.py
@@ -13,3 +13,25 @@ for i in range(3,9):
  
  
 print(suppliers)
+
+transaction_amounts = []
+ 
+for i in suppliers:
+    transaction_amount = 0
+    for j in range(3,9):
+        supplier = sheet.cell(row=j, column=2).value
+ 
+        if supplier == i :
+            transaction_amount += sheet.cell(row=j, column=6).value
+ 
+    transaction_amounts.append(transaction_amount)
+new_sheet = workbook.create_sheet("集計")
+ 
+new_sheet["A1"] = "取引先"
+new_sheet["B1"] = "取引金額"
+ 
+for i, j, k in zip(list(range(3,9)), suppliers, transaction_amounts):
+    new_sheet.cell(row=i, column=1, value=j)
+    new_sheet.cell(row=i, column=2, value=k)
+ 
+workbook.save("torihiki.xlsx")
