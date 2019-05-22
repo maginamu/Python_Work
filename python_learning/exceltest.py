212ea35654586ff0fde2ec4afed31d2d31992077
maginamu <magi.namu@gmail.com>
2019-05-23 00:49:10 +0900

    test

--- python_learning/exceltest.py
+++ python_learning/exceltest.py
@@ -1,1 +1,15 @@
-print('Hello')
+import openpyxl
+
+workbook = openpyxl.load_workbook('torihiki.xlsx')
+sheet = workbook["Sheet1"]
+
+suppliers = []
+ 
+for i in range(3,9):
+    cell_value = sheet.cell(row=i, column=2).value
+ 
+    if cell_value not in suppliers:
+        suppliers.append(cell_value)
+ 
+ 
+print(suppliers)
