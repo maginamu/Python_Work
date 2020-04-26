7ebedbe07723fd2436876c7244c1570334546366
SubOrigin <magi.namu@gmail.com>
2020-04-27 01:39:18 +0900

    行数自由にできるように修正

--- exe/anke-to.py
+++ exe/anke-to.py
@@ -2,8 +2,7 @@ import openpyxl
 import os
 import sys
 
-fileno = 1
-hanni = "B54:B59"
+fileno = 2 #集計結果は2行目から
 
 testpass = input("集計対象のファイルがあるパスを入力してください。:")
 print(testpass)
@@ -11,26 +10,30 @@ print(testpass)
 testhanni = input("集計対象のセルを入力（B54:B59）:")
 print(testhanni)
 
+#パスの中にあるファイル名を取得
 files = os.listdir(testpass+'\\')
 print(files)
+print('ファイル数:'+str(len(files)))
 
 hantei = input("集計開始する場合は、1を入力:")
 if hantei != '1':
     print('実行を中止します')
     sys.exit(1)
 
-
-wb_result = openpyxl.Workbook()
-sheet_result = wb_result.active
+wb_result = openpyxl.Workbook() #集計結果Book ブックハンドラ取得
+sheet_result = wb_result.active #集計結果Book シートハンドラ取得
 sheet_result.title = '集計結果'
+sheet_result.cell(1,1,"ファイル名") #1行目に項目を記載
+sheet_result.cell(1,2,testhanni)
 
-for fcnt in files:
-    wb = openpyxl.load_workbook(testpass+'\\'+fcnt)
-    sheet_result.cell(fileno,1,fcnt)
-    ws = wb["Sheet1"]
-    rng1 = ws[testhanni]
-    for cnt in range(6):
-        sheet_result.cell(fileno, cnt+2, rng1[cnt][0].value)
+for fcnt in files: #取得したファイル数分でループ
+    wb = openpyxl.load_workbook(testpass+'\\'+fcnt) #ファイルオープン
+    sheet_result.cell(fileno,1,fcnt) #A列にファイル名記載
+    ws = wb.active #開いたシートに結果がある前提
+    colno = 2  # B列から入力
+    for rows in ws[testhanni]:
+        sheet_result.cell(fileno, colno, rows[0].value)
+        colno+=1
     fileno+=1
 
 wb_result.save('C:\\Users\\magin\\Desktop\\Tasks\\200410_アンケート収集ツール作成\\集計結果.xlsx')
